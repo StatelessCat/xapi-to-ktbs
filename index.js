a6439b6d1d48d222f965a7b14afdbeb5b2494b5e
Raphaël Cazenave-Lévêque <raphael.cazenave@gmail.com>
2016-05-04 16:45:14 +0200

    Parse the Statement 1 in JSON-LD, get a very tiny subset of it, push it to the kTBS as an Obsel using JSON-LD

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 /* eslint-disable no-console */
 const fs = require('fs');
 const ktbs = require('./ktbs');
+const jsonld = require('jsonld');
 
 const EVAL_1_JSONLD_PATH =
-      './resources/jsonld-obsel.jsonld';
+      './resources/tincan2prov/evaluation-1-jsonld.jsonld';
+      //      './resources/jsonld-obsel.jsonld';
 const s1 = fs.readFileSync(EVAL_1_JSONLD_PATH, 'utf8');
 const XAPI_TO_KTBS_MODEL_PATH = './resources/xapi-ktbs-model.ttl';
 
 const model = fs.readFileSync(XAPI_TO_KTBS_MODEL_PATH, 'utf8');
+
+var ss1 = JSON.parse(s1);
+
+var ss2 = {
+  "@id": ss1.id,
+  "@type": ["m:xapiStatement", ss1["@type"]],
+  "hasTrace": "./",
+  "@context": [
+    ss1["@context"],
+    { "m": "http://localhost:8001/base1/m1#" },
+    "http://liris.cnrs.fr/silex/2011/ktbs-jsonld-context"
+  ]
+};
+console.log('==ss2=raw==');
+console.log(JSON.stringify(ss2, null, 2));
+console.log('===========');
+
 
 ktbs.deleteX({
   path: '/base1/m1'
@@ -31,12 +50,15 @@ ktbs.postTrace({
   tracename: 't1/',
   hasModel: '/base1/m1'
 });
-console.log('====');
-console.log(s1);
-console.log('====');
 ktbs.postObsel({
   path: '/base1/t1/',
-  payload: s1,
+  payload: JSON.stringify(ss2),
   headers: {'Content-Type': 'application/ld+json'}
+});
+
+jsonld.flatten(ss2, function(err, flattened) {
+  console.log('==flattened==');
+  console.log(JSON.stringify(flattened, null, 2));
+  console.log('=============');
 });
 
