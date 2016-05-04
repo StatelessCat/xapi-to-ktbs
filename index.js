8be205389caf2eb63628f9773ecb7ada72cd6a45
Raphaël Cazenave-Lévêque <raphael.cazenave@gmail.com>
2016-05-04 09:53:58 +0200

    Statement1 --jsonld.js-> quads --n3.js-> turtle

--- index.js
+++ index.js
@@ -1,48 +1,36 @@
+/* eslint-disable no-console */
 const fs = require('fs');
 const ktbs = require('./ktbs');
-const jsonld = require('jsonld');
 
-const EVAL_1_JSONLD_PATH =
-      './resources/tincan2prov/evaluation-1-jsonld.jsonld';
 const XAPI_TO_KTBS_MODEL_PATH = './resources/xapi-ktbs-model.ttl';
 
-const s1 = fs.readFileSync(EVAL_1_JSONLD_PATH, 'utf8');
 const model = fs.readFileSync(XAPI_TO_KTBS_MODEL_PATH, 'utf8');
 
-jsonld.normalize(JSON.parse(s1), {
-  algorithm: 'URDNA2015',
-  format: 'application/nquads',
-}, function(err, normalized) {
-  console.log('==============');
-  console.log(normalized);
-  console.log('==============');
-});
-
 ktbs.deleteX({
-  path: '/base1/m1',
+  path: '/base1/m1'
 });
 ktbs.deleteX({
-  path: '/base1/t1/',
+  path: '/base1/t1/'
 });
 ktbs.deleteX({
-  path: '/base1/',
+  path: '/base1/'
 });
 ktbs.postBase({
-  basename: 'base1/',
+  basename: 'base1/'
 });
 ktbs.postModel({
   path: '/base1/',
   headers: {'Content-Type': 'text/turtle'},
-  payload: model,
+  payload: model
 });
 ktbs.postTrace({
   basepath: '/base1/',
   tracename: 't1/',
-  hasModel: '/base1/m1',
+  hasModel: '/base1/m1'
 });
-ktbs.postObsel({
-  path: '/base1/t1/',
-  payload: s1,
-  headers: {'Content-Type': 'application/ld+json'},
-});
+//ktbs.postObsel({
+//  path: '/base1/t1/',
+//  payload: s1,
+//  headers: {'Content-Type': 'application/ld+json'}
+//});
 
---
+++ jsonld-test.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+const fs = require('fs');
+const jsonld = require('jsonld');
+const N3 = require('n3');
+
+const EVAL_1_JSONLD_PATH =
+      './resources/tincan2prov/evaluation-1-jsonld.jsonld';
+const s1 = fs.readFileSync(EVAL_1_JSONLD_PATH, 'utf8');
+
+jsonld.normalize(JSON.parse(s1), {
+  algorithm: 'URDNA2015',
+  format: 'application/nquads'
+}, function(err, normalized) {
+  console.log('==Normalised==');
+  console.log(normalized);
+  console.log('==============');
+  var parser = N3.Parser();
+  var writer = N3.Writer();
+  parser.parse(normalized, function (error, triple) {
+    if (triple) {
+      writer.addTriple({
+        subject:   triple.subject,
+        predicate: triple.predicate,
+        object:    triple.object
+      });
+    }
+    else {
+      console.log('====Turtle====');
+      writer.end(function (error, result) { console.log(result); });
+      console.log('==============');
+    }
+  });  
+});
+
