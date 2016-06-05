aba5f80fa93d77fe5fa44d74cd404c4fef9b19b4
Raphaël Cazenave-Lévêque <raphael.cazenave@gmail.com>
2016-06-05 16:32:52 +0200

    Use JSON-LD framing to restructure the obsel like the original xAPI JSON-LD statement

--- validate.js
+++ validate.js
@@ -2,15 +2,20 @@
 /* globals Promise:true */
 const http = require('http');
 const N3 = require('n3');
+const fs = require('fs');
 var jsonld = require('jsonld');
 
 var n3parser = N3.Parser();
+
+const FRAME_PATH = './resources/framing/frame.json';
+var frame_str = fs.readFileSync(FRAME_PATH, 'utf8');
+var frame_ = JSON.parse(frame_str);
 
 new Promise(function(resolve, reject) {
   http.get({
     hostname: 'localhost',
     port: 8001,
-    path: '/b1/t1/02abc888-2672-45bb-8256-f01efdda6660',
+    path: '/b1/t1/be3ece5e-477b-4f02-be8e-5badb5ca9873',
     method: 'GET',
     headers: {
       'Accept': 'text/plain'
@@ -34,10 +39,17 @@ new Promise(function(resolve, reject) {
     jsonld.fromRDF(ntriples, {format: 'application/nquads'}, function(err, doc) {
       console.log(err);
       // console.log(JSON.stringify(doc, null, 2));
-      jsonld.compact(doc, {}, function(err, compacted) {
-        console.log("------------");
-        console.log(JSON.stringify(compacted, null, 2));
-        console.log("------------");
+
+      jsonld.frame(doc, frame_, function(err, framed) {
+        fs.writeFile('statement-1-jsonld-out-framed.json', JSON.stringify(framed, null, 2), (err) => {
+          if (err) throw err;
+          console.log('It\'s saved!');
+        });
+      });
+
+      fs.writeFile('statement-1-jsonld-out.json', JSON.stringify(doc, null, 2), (err) => {
+        if (err) throw err;
+        console.log('It\'s saved!');
       });
     });
   });
