9911fd96c8b97d7068417ab8a6f13a8e416b6568
Raphaël Cazenave-Lévêque <raphael.cazenave@gmail.com>
2016-07-21 00:42:18 +0200

    transformation creation ok

--- xapi-to-prov.js
+++ xapi-to-prov.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-console */
 var Hjson = require('hjson');
 const fs = require('fs');
 
 var computed_trace_uri = process.argv[2] || 'prov_trace42/';
+var computed_trace_model_uri = process.argv[3] || 'http://localhost:8001/b1/nomodel';
 
 if (!process.argv[2]) {
   console.log('node xapi-to-prov.js <computed trace URI>');
@@ -19,12 +21,21 @@ const transformation = {
   "hasSource": [ "t1/" ],
   "hasMethod": "k:translation"
 };
-let parameters = mapping_array.map(({xapi, prov}) => ({[xapi]: prov}));
+
 // format parameters to something like   "parameter": [
 //    "model=http://localhost:8001/b1/nomodel",
-//    "map={\"http://semweb.mmlab.be/ns/tincan2prov/actor\": \"http://www.w3.org/ns/prov#Agent\"}"
+//    "map={\"http://semweb.mmlab.be/ns/tincan2prov/actor\": \"http://www.w3.org/ns/prov#Agent\",\"http://semweb.mmlab.be/ns/tincan2prov/object\": \"http://www.w3.org/ns/prov#Entity\"}"
 //  ]
+var x = mapping_array.map(({xapi, prov}) => "\""+xapi+"\": \""+prov+"\"");
+let map = "map={" + x.join(",") + "}";
+let model = "model=" + computed_trace_model_uri;
+let parameter = [model, map];
+console.log(parameter);
 
 // add it to the transformation object litteral
+transformation.parameter = parameter;
+
 // send the transformation to the kTBS
+console.log(JSON.stringify(transformation, null, 2));
+
 
